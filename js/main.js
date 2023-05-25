9047d00ce2857620f48335ca25635ff803e0a6df
MaciekDuda <maciejduda1626@gmail.com>
2023-05-25 17:00:39 +0200

    js

--- js/main.js
+++ js/main.js
@@ -11,19 +11,53 @@ const popup = document.querySelector('form-popup');
 
 const inputs = [usernameInput, passwordInput, repasswordInput, emailInput];
 
-const showError = (input, msg) => { 
+const showError = (input, msg) => {
+	const formBox = input.parentElement;
+	console.log(formBox);
+	const errorMsg = formBox.querySelector('.form__box-error-text');
+	errorMsg.textContent = msg;
+	formBox.classList.add('error');
+};
 
- }
-
+const clearError = (input) => {
+	const formBox = input.parentElement;
+	formBox.classList.remove('error');
+};
 
 const checkForm = (input) => {
-	input.forEach(el => {
+	input.forEach((el) => {
 		if (el.value === '') {
 			showError(el, el.placeholder);
 		} else {
-			console.log('ok');
+			clearError(el);
 		}
-	})
+	});
+};
+
+const checkLength = (input, min) => {
+	if (input.value.length < min) {
+		const labelText = input.previousElementSibling.innerText
+			.trim()
+			.replace(/:$/, '');
+		showError(input, `${labelText} powinna składać się z ${min} znaków.`);
+	}
+};
+
+const checkPassword = (pass1, pass2) => {
+	if (pass1.value !== pass2.value) {
+		showError(pass2, 'Podane hasła są różne');
+	}
+};
+
+const checkMail = (email) => {
+	const re =
+		/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+	if (re.test(email.value)) {
+		clearError(error);
+	} else {
+		showError(email, 'Błędny format email');
+	}
 };
 
 clearBtn.addEventListener('click', (e) => {
@@ -36,6 +70,9 @@ clearBtn.addEventListener('click', (e) => {
 
 submitBtn.addEventListener('click', (e) => {
 	e.preventDefault();
-
 	checkForm(inputs);
+	checkLength(usernameInput, 3);
+	checkLength(passwordInput, 8);
+	checkPassword(passwordInput, repasswordInput);
+	checkMail(emailInput);
 });
